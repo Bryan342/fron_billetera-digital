83691498157bcb0cec4824dfdd884f5f6190ef94
FabrizioPLC <fabrizio0943@gmail.com>
2025-11-21 00:14:29 -0500

    boton de logout funcional

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import '../styles/sidebar.css';
 import logo from '../assets/logo.png';
 
 function Sidebar() {
   const location = useLocation();
+  const navigate = useNavigate();
   const isHomeActive = location.pathname === '/home' || location.pathname === '/'; 
+
+  const handleLogout = () => {
+    localStorage.clear();
+
+    navigate("/"); 
+  };
 
   return (
     <div className="sidebar-container">
       
-      {/* Sección Superior: Logo y Título */}
       <div className="sidebar-header">
         <img src={logo} alt="Logo Luca" className="sidebar-logo-img" /> 
         <h3 className="sidebar-title">Luca</h3>
       </div>
 
-      {/* Sección de Navegación Principal */}
       <nav className="sidebar-nav">
         <ul className="nav-list">
-          
-          {/* Ítem: Inicio */}
+
           <li className={`nav-item ${isHomeActive ? 'active' : ''}`}>
             <Link to="/home" className="nav-link">
               <span className="icon">🏠</span> 
@@ -28,7 +32,6 @@ function Sidebar() {
             </Link>
           </li>
 
-          {/* Ítem: Billetera */}
           <li className={`nav-item ${location.pathname === '/billetera' ? 'active' : ''}`}>
             <Link to="/billetera" className="nav-link">
               <span className="icon">💳</span> 
@@ -36,24 +39,24 @@ function Sidebar() {
             </Link>
           </li>
 
-          {/* Ítem: Enviar */}
           <li className={`nav-item ${location.pathname === '/enviar' ? 'active' : ''}`}>
             <Link to="/enviar" className="nav-link">
               <span className="icon">✉️</span> 
               Enviar
             </Link>
           </li>
+
         </ul>
       </nav>
 
-      {/* Sección Inferior: Salir */}
       <div className="sidebar-footer">
         <div className="footer-box">
-           <button className="btn-options">
-               <span className="icon">⚙️</span>
-           </button>
+          <button className="btn-options">
+            <span className="icon">⚙️</span>
+          </button>
         </div>
-        <button className="btn-logout">
+
+        <button className="btn-logout" onClick={handleLogout}>
           Salir
         </button>
       </div>
